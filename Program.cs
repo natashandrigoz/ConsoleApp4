01bf91bc9aa9ead9206b7eaa6d93523b33165740
natashandrigoz <nata_shandrigoz@mail.ru>
2023-03-08 06:39:39 +0200

    Пример 2. Вызов пустого метода без возвращения значений

--- Program.cs
+++ Program.cs
@@ -8,19 +8,39 @@ namespace ConsoleApp4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Пример 1. Создание простого метода и его вызов
+        //static void Main(string[] args)
+        //{
+        //    SayHello();
+        //    SayGoodbye();
+        //    Console.Read();
+        //}
+        //static void SayHello()
+        //{
+        //    Console.WriteLine("Hello!");
+        //}
+        //static void SayGoodbye()
+        //{
+        //    Console.WriteLine("GoodBye!");
+        //}
+        //Пример 2. Вызов пустого метода без возвращения значений
+        /// <summary>
+        /// Дополнительный метод Func()
+        /// </summary>
+        static void Func()
         {
-            SayHello();
-            SayGoodbye();
+            Console.Write("x= ");
+            double x = double.Parse(Console.ReadLine());
+            double f = Math.Pow(x, 2);
+            Console.WriteLine($"f({x})= {f}");
+        }
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        static void Main()
+        {
+            Func();
             Console.Read();
-        }
-        static void SayHello()
-        {
-            Console.WriteLine("Hello!");
-        }
-        static void SayGoodbye()
-        {
-            Console.WriteLine("GoodBye!");
         }
     }
 
