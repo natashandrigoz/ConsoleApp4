565ee7f445f86795d7a264567806d49b045750bc
natashandrigoz <nata_shandrigoz@mail.ru>
2023-03-08 20:29:49 +0200

    Найти периметр и площадь треугольника, заданного координатами своих вершин. Определить метод для расчета длины отрезка по координатам его вершин

--- Program.cs
+++ Program.cs
@@ -7,32 +7,59 @@ using System.Threading.Tasks;
 namespace ConsoleApp4
 {
     /// <summary>
-    /// Найти s=max(a, b) + max(c, d)
+    /// Найти периметр и площадь треугольника, заданного координатами своих вершин. 
+    /// Определить метод для расчета длины отрезка по координатам его вершин
     /// </summary>
     internal class Program
     {
         ///<summary>
-        ///Доп. метод нахождения максимальной величины
+        ///Доп. метод для расчета длины отрезка по координатам его вершин
         ///</summary>
-        static double Max(double x, double y)
+        static double Dlina(double x1, double y1,double x2,double y2)
         {
-            return (x > y) ? x : y;
+            return Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2));
+        }
+        ///<summary>
+        ///Доп. метод для расчета периметра треугольника, 
+        ///заданного координатами своих вершин
+        ///</summary>
+        static double Perimetr(double a,double b, double c)
+        {
+            return a + b + c;
+        }
+        ///<summary>
+        ///Доп. метод для расчета площади треугольника, 
+        ///заданного координатами своих вершин
+        ///</summary>
+        static double Square(double a, double b, double c)
+        {
+            double pp = (a + b + c) / 2;
+            return Math.Sqrt(pp * (pp - a) * (pp - b) * (pp - c));
         }
         /// <summary>
         /// Точка входа в программу
         /// </summary>
         static void Main()
         {
-            Console.Write("a= ");
-            double a = Convert.ToDouble(Console.ReadLine());
-            Console.Write("b= ");
-            double b = Convert.ToDouble(Console.ReadLine());
-            Console.Write("c= ");
-            double c = Convert.ToDouble(Console.ReadLine());
-            Console.Write("d= ");
-            double d = Convert.ToDouble(Console.ReadLine());
-            double s = Max(a, b) + Max(c, d);
-            Console.WriteLine($"max({a},{b})+max({c},{d})={s}");
+            Console.WriteLine("Введите координаты первой вершины:");
+            Console.Write("x1= ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("y1= ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координаты второй вершины:");
+            Console.Write("x2= ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("y2= ");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координаты третьей вершины:");
+            Console.Write("x3= ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("y3= ");
+            double y3 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Периметр треугольника, заданного координатами своих вершин, = " +
+                $"{Perimetr(Dlina(x1,y1,x2,y2),Dlina(x2,y2,x3,y3),Dlina(x1,y1,x3,y3)):f2}");
+            Console.WriteLine($"Площадь треугольника, заданного координатами своих вершин, = " +
+                $"{Square(Dlina(x1, y1, x2, y2), Dlina(x2, y2, x3, y3), Dlina(x1, y1, x3, y3)):f2}");
             Console.Read();
         }
     }
